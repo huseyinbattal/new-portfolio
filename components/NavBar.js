516bbea3b1a5211fc67fb9d664bc39fc00616abc
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-11 22:50:49 +0300

    design mobile menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -53,8 +53,27 @@ const NavBar = () => {
               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"> 
                 <AiOutlineClose />
               </div>
-            </div>
-          </div>
+                      </div>
+                      <div className="border-b border-gray-300 my-4 ">
+                        <p className="w-[85%] md:w-[90%] py-4">Let`s build something legendary together</p>
+                      </div>
+                  </div>
+                  <div className="py-4 flex flex-col">
+                      <ul className="uppercase">
+                          <Link href="/">
+                          <li className="py-4 text-sm">Home</li>
+                          </Link>
+                          <Link href="/">
+                          <li className="py-4 text-sm">About</li>
+                          </Link><Link href="/">
+                          <li className="py-4 text-sm">Skills</li>
+                          </Link><Link href="/">
+                          <li className="py-4 text-sm">Project</li>
+                          </Link><Link href="/">
+                          <li className="py-4 text-sm">Contact</li>
+                          </Link>
+                      </ul>
+                  </div>
         </div>
       </div>
     </div>
